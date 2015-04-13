80805b16f2878805ee64f31e8186bd46cecf875b
Rafael Ferreira <mail@rafaelferreira.pt>
2015-04-13 10:15:26 +0100

    ex b) e c)

--- Aula 7/ex71.sql
+++ Aula 7/ex71.sql
@@ -7,14 +7,10 @@ CREATE VIEW TITLE_AUTHOR AS
 		pubs.dbo.titleauthor JOIN pubs.dbo.authors ON titleauthor.au_id = authors.au_id
 	) ON titleauthor.title_id = titles.title_id);
 
-SELECT * FROM TITLE_AUTHOR;
-
 -- ex1 a) ii)
 CREATE VIEW FUNC_EDITORS AS
 	SELECT publishers.pub_name, employee.fname + ' ' + employee.minit + ' ' + employee.lname AS name
 	FROM pubs.dbo.publishers JOIN pubs.dbo.employee ON publishers.pub_id=employee.pub_id
-
-SELECT * FROM FUNC_EDITORS
 
 -- ex1 a) iii)
 CREATE VIEW STORES_TITLES AS
@@ -22,12 +18,24 @@ CREATE VIEW STORES_TITLES AS
 	(pubs.dbo.titles JOIN (pubs.dbo.sales JOIN pubs.dbo.stores ON sales.stor_id = stores.stor_id) 
 	ON titles.title_id = sales.title_id)
 
-SELECT * FROM STORES_TITLES;
-
 -- ex1 a) iv)
 CREATE VIEW BUSINESS_BOOKS AS
 	SELECT titles.title
 	FROM pubs.dbo.titles
 	WHERE titles.type = 'Business'
 
-SELECT * FROM BUSINESS_BOOKS
+-- ex1 b)
+SELECT * FROM TITLE_AUTHOR;
+SELECT * FROM FUNC_EDITORS;
+SELECT * FROM STORES_TITLES;
+SELECT * FROM BUSINESS_BOOKS;
+
+-- ex1 c)
+CREATE VIEW NAME_STORES_AUTHORS AS
+	(SELECT stor_name, name FROM
+		TITLE_AUTHOR, STORES_TITLES
+		WHERE TITLE_AUTHOR.title = STORES_TITLES.title
+	);
+
+SELECT * FROM NAME_STORES_AUTHORS ORDER BY stor_name, name ASC;
+
