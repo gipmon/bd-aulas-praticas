4c06a8e813c66b64a1ddd6bc737b16239c088a17
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-18 09:33:23 +0100

    aula 11 a

---
+++ Aula 11/problema11_1.sql
@@ -0,0 +1,26 @@
+use p4g5;
+
+go
+-- ex a)
+CREATE PROCEDURE company.sp_remove_employee
+	@Ssn int
+WITH ENCRYPTION
+AS
+	BEGIN TRANSACTION;
+
+	BEGIN TRY
+		DELETE FROM company.dependent WHERE dependent.Essn = @Ssn;
+		DELETE FROM company.works_on WHERE works_on.Essn = @Ssn;
+		DELETE FROM company.employee WHERE employee.Ssn = @Ssn;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		RAISERROR ('An error occurred when removing the employee!', 14, 1)
+		ROLLBACK TRANSACTION;
+	END CATCH;
+
+go
+EXEC company.sp_remove_employee @Ssn = null;
+
+go
+-- ex b)
