fc719315dd713f0cda4080d3fd48c75271a7e5b3
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-05-18 18:21:50 +0100

    problemas

--- Aula 11/problema11_1.sql
+++ Aula 11/problema11_1.sql
@@ -82,7 +82,6 @@ AS
 
 	SELECT @salary = employee.Salary FROM company.employee JOIN (company.department JOIN inserted ON department.Dnumber = inserted.Dno) ON employee.Ssn = department.Mgr_ssn;
 
-	PRINT @salary
 	DECLARE @newSalary money;
 	DECLARE @ssn int;
 
@@ -110,3 +109,57 @@ AS
 
 go
 SELECT * FROM company.employee_projects(183623612);
+
+
+-- f)
+
+go
+
+CREATE FUNCTION company.employee_salary(@dnumber int=null)
+RETURNS @table TABLE ("fname" varchar(50), "minit" varchar(50), "lname" varchar(50), "ssn" int, "salary" money)
+WITH SCHEMABINDING, ENCRYPTION
+AS
+	BEGIN
+		DECLARE @media int;
+		SELECT @media = AVG(employee.salary) FROM company.employee WHERE employee.Dno = @dnumber;
+
+		INSERT @table SELECT employee.Fname, employee.Minit, employee.Lname, employee.Ssn, employee.Salary 
+					  FROM company.employee where employee.Dno = @dnumber AND employee.Salary > @media;
+		RETURN;
+	END;
+
+go
+SELECT * FROM company.employee_salary(3);
+
+-- h)
+
+go
+
+-- DROP TRIGGER trigger_delete_department
+CREATE TRIGGER trigger_delete_department ON company.department
+INSTEAD OF DELETE
+AS
+	SET NOCOUNT ON;
+	IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
+				WHERE TABLE_SCHEMA = 'company' AND TABLE_NAME = 'department_deleted'))
+		BEGIN
+			INSERT INTO department_deleted SELECT * FROM deleted;
+			SELECT * from deleted;
+			END;
+
+	ELSE
+		BEGIN
+			CREATE TABLE company.department_deleted(
+				Dnumber int PRIMARY KEY,
+				Dname varchar(180) not null,
+				Mgr_ssn int,
+				Mgr_start_date date);
+
+			ALTER TABLE company.department_deleted ADD CONSTRAINT DELDEP
+			FOREIGN KEY (Mgr_ssn) REFERENCES company.employee
+			SELECT * from deleted;
+			INSERT INTO department_deleted SELECT * FROM deleted;
+			END;
+go
+
+DELETE FROM company.department WHERE department.Dnumber = 3
